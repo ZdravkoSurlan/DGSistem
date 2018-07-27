21748adc21ba9f025721003ff146c9b3faaaa0be
ZdravkoSurlan <41573832+ZdravkoSurlan@users.noreply.github.com>
2018-07-27 14:02:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+<script>
+		/* Set the width of the side navigation to 250px */
+		function openNav() {
+			document.getElementById("mySidenav").style.width = "250px";
+			closeOnCLick: true;
+		}
+
+		/* Set the width of the side navigation to 0 */
+		function closeNav() {
+			document.getElementById("mySidenav").style.width = "0";
+			closeOnCLick: true;
+		}
+	</script>
